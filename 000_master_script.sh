2687506406b22585c1e00688c3aaadf5e01fb2f6
Kevin Rue-Albrecht <k.rue-albrecht@imperial.ac.uk>
2016-03-23 14:47:22 +0000

    Add master script showing pipeline of commands

---
+++ 000_master_script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Check whether the md5 signature matches the original one
+./001_md5.sh rawdata > out.001
+
+# Control quality of the raw reads
+./002_fastqc.sh trim_galore '*.fastq.gz' fastqc 12 > out.002
+
+# Collate the output of FastQC in a friendly CSV file
+./003_fastqc_summarisation.sh rawdata > out.003
+
+# Trim adapter and low quality nucleotides
+./004_trimgalore.sh rawdata trim_galore 12 > out.004
+
+# Control quality of the raw reads
+./005_fastqc.sh trim_galore '*.fq.gz' fastqc_trimmed 12 > out.005
+
+# Collate the output of FastQC in a friendly CSV file
+./006_fastqc_trimmed_summarisation.sh fastqc_trimmed > out.006
+
+# Collate the output of trim_galore in a friendly CSV file
+./007_trimgalore_summarisation.sh fastqc_trimmed > out.007
+
+# Align bisulfite libraries (single-end, paired-end, trimmed unpaired)
+./008_bismark_align.sh <genome> <rootdir> <outdir> <theads> <tempdir>
