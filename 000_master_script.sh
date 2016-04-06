084b9f60fa433b7c651215a901489c5c47f54200
Kevin Rue-Albrecht <k.rue-albrecht@imperial.ac.uk>
2016-04-06 18:36:52 +0100

    Updated master script

--- 000_master_script.sh
+++ 000_master_script.sh
@@ -18,8 +18,8 @@
 # TODO Collate the output of FastQC in a friendly CSV file
 #006
 
-# TODO Trim adapter and low-quality bases
-./007_trimgalore.sh
+# Trim adapter and low-quality bases
+./007_trimgalore.sh trimgalore 12 rawdata/Merged > out.007
 
 # TODO Control quality of the trimmed reads
 #008
@@ -27,8 +27,37 @@
 # TODO Collate the output of FastQC in a friendly CSV file
 #009
 
-# TODO Collate trimming statistics
+# Collate trimming statistics
 ./010_trimgalore_summarisation.sh trimgalore > out.010
 
 # TODO Align trimmed reads of bisulfite libraries
 ./011_bismark.sh bostaurus trimgalore bismark 4 tmp_bismark > out.011
+
+# TODO Control quality of the aligned reads
+#012
+
+# TODO Collate the output of FastQC in a friendly CSV file
+#013
+
+# Collate alignment statistics
+./014_bismark_summarisation.sh bismark > out.014
+
+# Deduplicate aligned reads / pairs
+./015_deduplicate.sh bismark 8 > out.015
+
+# TODO Control quality of the deduplicated reads
+#016
+
+# TODO Collate the output of FastQC in a friendly CSV file
+#017
+
+# Extract methylation calls from deduplicated aligned reads / pairs
+./018_extractor.sh bismark extract 8 > out.018
+
+
+
+
+
+
+
+
