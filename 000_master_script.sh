1e251890fe0c397196173c559c85a0648b71ce62
Kevin Rue-Albrecht <k.rue-albrecht@imperial.ac.uk>
2016-04-15 15:23:42 +0100

    Combined FastQC and summarisation scripts. Reordered resulting scripts.

--- 000_master_script.sh
+++ 000_master_script.sh
@@ -4,64 +4,56 @@ l#!/bin/bash
 ./001_md5.sh rawdata > out.001
 
 # Control quality of the raw reads
-./002_fastqc.sh rawdata '*.fastq.gz' fastqc 12 > out.002
-
-# Collate the output of FastQC in a friendly CSV file
-./003_fastqc_summarisation.sh rawdata > out.003
+./002_fastqc_raw.sh -t 12 rawdata '*.fastq.gz' fastqc_raw > out.002
 
 # Merge all paired Fastq files
-./004_merge_fastq.sh rawdata rawdata/Merged 12 > out.004
+./003_merge_fastq.sh rawdata rawdata/Merged 12 > out.004
 
 # Control quality of the merged raw reads
-./005_fastqc.sh rawdata/Merged *fastq.gz fastqc_merged 12 > out.005
-
-# Collate the output of FastQC in a friendly CSV file
-./006_fastqc_summarisation.sh fastqc_merged > out.006
+./004_fastqc_merged.sh -t 12 rawdata/Merged *fastq.gz fastqc_merged 12 > out.004
 
 # Trim adapter and low-quality bases
-./007_trimgalore.sh trimgalore 12 rawdata/Merged > out.007
+./005_trimgalore.sh trimgalore 12 rawdata/Merged > out.005
 
 # Control quality of the trimmed reads
-./008_fastqc.sh trimgalore/Merged *fq.gz fastqc_trimmed 12 > out.008
-
-# Collate the output of FastQC in a friendly CSV file
-./009_fastqc_summarisation.sh fastqc_trimmed > out.009
+./006_fastqc_trimmed.sh -t 12 trimgalore/Merged *fq.gz fastqc_trimmed > out.006
 
 # Collate trimming statistics
-./010_trimgalore_summarisation.sh trimgalore > out.010
+./007_trimgalore_summarisation.sh trimgalore > out.007
 
 # Align trimmed reads of bisulfite libraries
-./011_bismark.sh bostaurus trimgalore bismark 4 tmp_bismark > out.011
+./008_bismark.sh bostaurus trimgalore bismark 4 tmp_bismark > out.008
 
 # TODO Control quality of the aligned reads
-#012
-
-# TODO Collate the output of FastQC in a friendly CSV file
-#013
+#009
 
 # Collate alignment statistics
-./014_bismark_summarisation.sh bismark > out.014
+./010_bismark_summarisation.sh bismark > out.010
 
 # Deduplicate aligned reads / pairs
-./015_deduplicate.sh bismark 8 > out.015
+./011_deduplicate.sh bismark 8 > out.011
 
 # Control quality of the deduplicated reads
-./016_fastqc.sh bismark/Merged *deduplicated.bam fastqc_deduplicated 12 > out.016
+./012_fastqc_deduplicated.sh -t 12 bismark/Merged *deduplicated.bam fastqc_deduplicated > out.012
 
-# Collate the output of FastQC in a friendly CSV file
-./017_fastqc_summarisation.sh fastqc_deduplicated > out.017
+# Collate deduplication statistics
+./013_deduplicate_summarisation.sh bismark > out.013
 
 # Extract methylation calls from deduplicated aligned reads / pairs
-./018_extractor.sh bismark extract 8 > out.018
+./014_extractor.sh bismark extract 8 > out.014
 
 # Based on the M-bias plots
 # Extract methylation calls again ignoring 7 bp from the 5' of each mate
-./018_extractor.sh bismark extract 4 --ignore 7 --ignore_r2 7  > out.018.refined
+./014_extractor.sh bismark extract 4 --ignore 7 --ignore_r2 7  > out.014.refined
+
+# TODO Collate methylation extraction statistics
+#015
 
 # Sort, add read group, and index BAM files.
-./019_sort_index.sh bismark/Merged 12 > out.019
+./016_sort_index.sh bismark/Merged 12 > out.016
 
-
+# TODO Collate HTML report for Bismark
+#017
 
 
 
--- 006_fastqc_trimmed.sh
+++ 006_fastqc_trimmed.sh
@@ -3,31 +3,72 @@
 echo "$0"
 echo "$(date -I)"
 
-if [ $# -lt 4 ]; then
-	echo "Usage: $0 <rootdir> <target_file> <outdir> <threads> [CSVfile]"
+if [ $# -lt 3 ]; then
+	echo "Usage: $0 [OPTIONS] <rootdir> <target_file> <outdir>"
+	echo "
+Options:
+	-s	Skip FastQC step. Only summarise content of outdir in CSV
+		file
+	
+	-t	Number of threads. Only applicable to FastQC step.
+"
 	exit 1
 fi
 
 cwd=`pwd`
 echo "cwd: $cwd"
 
-rootdir=$1
-echo "rootdir: $rootdir"
-target_file=$2
-echo "target_file: $target_file"
-outdir=$3
-echo "outdir: $outdir"
-threads=$4
-echo "threads: $threads"
+while getopts "st:c:" opt; do
+	echo "@: $@"
+	echo "opt: $opt"
+	case $opt in
+		s)
+			fastqc=false
+			if [ -n "$threads" ]; then
+				echo "'-t' not applicable: FastQC step skipped."
+			fi
+			shift $((OPTIND-1)); OPTIND=1
+			;;
+		c)
+			CSVfile=$OPTARG
+			CSVfolder=$(dirname $CSVfile)
+			shift $((OPTIND-1)); OPTIND=1
+			;;
+		t)
+			threads=$OPTARG
+			if [ -n "$fastqc" ]; then
+				echo "'-t' not applicable: FastQC step skipped."
+			fi
+			shift $((OPTIND-1)); OPTIND=1
+			;;
+		\?)
+			echo "Invalid option: -$OPTARG" >&2
+			exit 1
+			;;
+		:)
+			echo "Option -$OPTARG requires an argument." >&2
+			exit 1
+			;;
+	esac
+done
 
-if [ -z $5 ]; then
-	outfolder='log'
-	CSVfile=$outfolder/"$(basename $0 | sed -e "s/\.sh/_$(date -I).csv/")"
-else
-	CSVfile=$2
-	outfolder=$(dirname $CSVfile)
+rootdir=$1; shift
+target_file=$1; shift
+outdir=$1; shift
+
+# Defaults
+if [ -z $fastqc ]; then
+	fastqc=true
 fi
-echo "CSVfile: $CSVfile"
+if [ -z $threads ]; then
+	threads=1
+fi
+if [ -z $CSVfolder ]; then
+	CSVfolder="log"
+fi
+if [ -z $CSVfile ]; then
+	CSVfile=$CSVfolder/"$(basename $0 | sed -e "s/\.sh/_$(date -I).csv/")"
+fi
 
 folders=`find $rootdir -name "$target_file" -exec dirname {} \; | sort | uniq`
 echo -e "folders (next line):\n$folders"
@@ -35,21 +76,28 @@ echo -e "folders (next line):\n$folders"
 if [ ! -e $outdir ]; then
 	mkdir -pv $outdir
 fi
+if [ ! -e $CSVfolder ]; then
+	mkdir -pv $CSVfolder
+fi
 
-for folder in `echo $folders`
-do
-	echo "folder: $folder"
-	# Need to separate batches of sequencing
-	# as some fastq files for the same sample have the same name
-	batch=$(basename $folder)
-	if [ ! -e $outdir/$batch ]; then
-		mkdir -pv $outdir/$batch
-	fi
-	fastqs=$(find $folder -name "$target_file" | xargs)
-	echo "Processing $(echo $fastqs | wc -w ) files..."
-	echo "fastqc --quiet --nogroup --extract --threads $threads --outdir $outdir/$batch $fastqs"
-	fastqc --quiet --nogroup --extract --threads $threads --outdir $outdir/$batch $fastqs
-done
+if $fastqc; then
+	for folder in `echo $folders`
+	do
+		echo "folder: $folder"
+		# Need to separate batches of sequencing
+		# as some fastq files for the same sample have the same name
+		batch=$(basename $folder)
+		if [ ! -e $outdir/$batch ]; then
+			mkdir -pv $outdir/$batch
+		fi
+		fastqs=$(find $folder -name "$target_file" | xargs)
+		echo "Processing $(echo $fastqs | wc -w ) files..."
+		echo "fastqc --quiet --nogroup --extract --threads $threads --outdir $outdir/$batch $fastqs"
+		fastqc --quiet --nogroup --extract --threads $threads --outdir $outdir/$batch $fastqs
+	done
+else
+	echo "Skipping FastQC. Jumping to summarisation into CSV file."
+fi
 
 echo "Collating FastQC reports.."
 
@@ -59,7 +107,7 @@ echo "\"Value\",\"QC\",\"File\",\"Sample\",\"Read\",\"Treatment\",\
 for folder in `ls $outdir`
 do
 	echo "folder: $folder"
-	for summaryfile in `find $rootdir/$batch -name summary.txt`
+	for summaryfile in `find $outdir/$folder -name summary.txt`
 	do
 		awk 'BEGIN {FS="\t"; OFS="\",\""} \
 		\
