f456352a5297fddcb2a7095f4c4c3fa545a7797e
Kevin Rue-Albrecht <k.rue-albrecht@imperial.ac.uk>
2016-04-27 23:40:58 +0100

    separate script to make a Genomic Range out of UCSC CpG island unmasked track

---
+++ 019_CGI_import.R
@@ -0,0 +1,30 @@
+
+
+# Set parameters ----------------------------------------------------------
+
+CpGislands.file <- "bostaurus/Bt_UMD31_CpG_islands_unmasked.bed"
+
+outdir <- 'bsseq'
+
+# Download CG islands -----------------------------------------------------
+
+# Manually download CpG islands tracks from:
+# http://genome.ucsc.edu/cgi-bin/hgTables
+# Downloaded both masked and unmasked for testing
+
+CpG.unmasked <- read.table(
+  file = CpGislands.file,
+  header = FALSE,
+  sep = "\t",
+  stringsAsFactors = FALSE)
+
+CpG.gr <- GRanges(
+  seqnames = CpG.unmasked[,1],
+  ranges = IRanges(
+    start = CpG.unmasked[,2],
+    end = CpG.unmasked[,3],
+    names = CpG.unmasked[,4]))
+
+rm(CpG.unmasked)
+
+saveRDS(object = CpG.gr, file = file.path(outdir, 'CpG.gr.rds'))
