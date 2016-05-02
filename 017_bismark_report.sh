41d10ac5d2bac4afe7a7bfa8a94f62947d9a989d
Kevin Rue-Albrecht <k.rue-albrecht@imperial.ac.uk>
2016-05-02 22:12:09 +0100

    Bismark2report called with all possible reports

--- 017_bismark_report.sh
+++ 017_bismark_report.sh
@@ -4,7 +4,7 @@ echo "$0"
 echo "$(date -I)"
 
 if [ $# -lt 3 ]; then
-	echo "Usage: $0 <rootdir> <outdir> <threads>"
+	echo "Usage: $0 <rootdir(s)> <outdir> <threads>"
 	exit 1
 fi
 
@@ -28,23 +28,27 @@ echo "alignmentReports: $(echo $alignmentReports | wc -w)"
 
 #dedupReports=$(echo $alignmentReports | sed 's/_PE_report.txt/_pe.deduplication_report.txt/g')
 dedupReports=$(find $rootdir -name "*_pe.deduplication_report.txt" | sort | xargs)
-#echo $dedupReports
+echo "dedupReports: $(echo $dedupReports | wc -w)"
 
 #splittingReports=$(echo $alignmentReports | sed 's/_PE_report.txt/_pe.deduplicated_splitting_report.txt/g')
 splittingReports=$(find $rootdir -name "*_pe.deduplicated_splitting_report.txt" | sort | xargs)
-#echo $splittingReports
+echo "splittingReports: $(echo $splittingReports | wc -w)"
 
 #mBiasReports=$(echo $alignmentReports | sed 's/_PE_report.txt/_pe.deduplicated.M-bias.txt/g')
 mBiasReports=$(find $rootdir -name "*_pe.deduplicated.M-bias.txt" | sort | xargs)
-#echo $mBiasReports
+echo "mBiasReports: $(echo $mBiasReports | wc -w)"
 
-echo "parallel -j $threads --xargs bismark2report --dir $outdir \
+#mBiasReports=$(TODO)
+nucleotideReports=$(find $rootdir -name "*_pe.nucleotide_stats.txt" | sort | xargs)
+echo "nucleotideReports: $(echo $nucleotideReports | wc -w)"
+
+echo "parallel -j $threads --xapply bismark2report --dir $outdir \
 	--alignment_report {1} --dedup_report {2} --splitting_report {3} \
-	--mbias_report {4} ::: $alignmentReports ::: $dedupReports ::: \
-	$splittingReports ::: $mBiasReports"
+	--mbias_report {4} --nucleotide_report {5} ::: $alignmentReports ::: $dedupReports ::: \
+	$splittingReports ::: $mBiasReports ::: $nucleotideReports"
 	
-parallel -j $threads --xargs bismark2report --dir $outdir \
+parallel -j $threads --xapply bismark2report --dir $outdir \
 	--alignment_report {1} --dedup_report {2} --splitting_report {3} \
-	--mbias_report {4} ::: $alignmentReports ::: $dedupReports ::: \
-	$splittingReports ::: $mBiasReports
+	--mbias_report {4} --nucleotide_report {5} ::: $alignmentReports ::: $dedupReports ::: \
+	$splittingReports ::: $mBiasReports ::: $nucleotideReports
 
